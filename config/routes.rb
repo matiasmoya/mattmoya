a3b6cf263ff10ea3a76702104fad8bae19b3656e
Matt Hinczuk <matiasmoya@gmail.com>
2015-02-25 13:16:59 -0300

    omniauth with facebook support

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for  :users, path: "", 
-              path_names: { sign_in: 'login', sign_out: 'logout', registration: 'account', sign_up: 'new'},
-              :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
 
   root 'home#index'
 end
