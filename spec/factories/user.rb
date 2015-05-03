734ca4c12b4bf18e48e1a69b4fa01ab6831dac67
Matt Hinczuk <matiasmoya@gmail.com>
2015-05-03 14:46:35 -0300

    Basic user specs

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,11 @@
 FactoryGirl.define do
   factory :user do
-    email Faker::Internet.email
+    email { Faker::Internet.email }
     password "asdasdasd"
     password_confirmation "asdasdasd"
+
+    trait :incorrect do
+      password_confirmation "123123123"
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,19 @@
 require "rails_helper"
 
 RSpec.describe User, :type => :model do
+
   it "Creates an user" do
     expect{
       @user = create(:user)
-    }.to change{ User.count }.by(1)
+      @user2 = create(:user)
+    }.to change{ User.count }.by(2)
+  end
+
+  it "Doesnt create an user with incorrect password_confirmation" do
+    expect{
+      create(:user, :incorrect)
+    }.to raise_error
+
+    expect(User.count).to eq(0)
   end
 end
